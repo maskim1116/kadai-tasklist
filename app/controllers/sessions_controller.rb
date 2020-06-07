03f00a9be4a782c8c529c4ae7fc1f3c6e25f4243
maskim <wamezarasi@gmail.com>
2020-06-07 08:33:42 +0000

    login&poct

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,35 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    email = params[:session][:email].downcase
+    password = params[:session][:password]
+    if login(email, password)
+      flash[:success] ='ログイン成功！'
+      redirect_to tasks_url
+    else
+      flash.now[:danger] = 'ログイン失敗'
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = 'ログアウト成功'
+    redirect_to login_path
+  end
+  
+  def login(email, password)
+    @user = User.find_by(email: email)
+    if @user && @user.authenticate(password)
+      #ログイン成功
+      session[:user_id] = @user.id
+      return true
+    else
+      #ログイン失敗
+      return false
+    end
+  end
+  
+end

7c4d69733fc4064dc3d3287be4e3c29709a5b0e4
maskim <wamezarasi@gmail.com>
2020-06-10 13:57:48 +0000

    heroku2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -58,7 +58,7 @@ class TasksController < ApplicationController
   end
   
   def correct_user
-    @task = current_user.tasks.find_by(Id: params[:id])
+    @task = current_user.tasks.find_by(id: params[:id])
     unless @task
       redirect_to root_url
     end
